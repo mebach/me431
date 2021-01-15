dff8f1360aff11a15261a9a55ffc1b326f043fe2
Mitchell Gehlbach <mitch.gehlbach@gmail.com>
2021-01-15 13:27:14 -0700

    yo

--- homework_template_folders/homework_template_folders/e_ballbeam/python/hw2/ballbeamSim.py
+++ homework_template_folders/homework_template_folders/e_ballbeam/python/hw2/ballbeamSim.py
@@ -2,33 +2,34 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 sys.path.append('..')  # add parent directory
-import massParam as P
+import ballbeamParam as P
 from signalGenerator import signalGenerator
-from massAnimation import massAnimation
+from ballbeamAnimation import ballbeamAnimation
 from dataPlotter import dataPlotter
 
+
 # instantiate reference input classes
-reference = signalGenerator(amplitude=1.0, frequency=0.2, y_offset=0.1)
-thetaRef = signalGenerator(amplitude=2.0*np.pi, frequency=0.1)
-tauRef = signalGenerator(amplitude=5, frequency=.5)
+reference = signalGenerator(amplitude=1.0, frequency=0.05, y_offset=0.1)
+zRef = signalGenerator(amplitude=0.25, frequency=0.05, y_offset=0.25)
+thetaRef = signalGenerator(amplitude=.1*np.pi, frequency=.025)
+fRef = signalGenerator(amplitude=5, frequency=.5)
 
 # instantiate the simulation plots and animation
 dataPlot = dataPlotter()
-animation = massAnimation()
+animation = ballbeamAnimation()
 
 t = P.t_start  # time starts at t_start
 while t < P.t_end:  # main simulation loop
     # set variables
-    z = reference.sin(t)
-    ctrl = 0.0
-
+    r = reference.sin(t)
+    z = zRef.sin(t)
+    theta = thetaRef.sin(t)
+    f = fRef.sin(t)
     # update animation
-
-    state = np.array([[z], [0.0]])
+    state = np.array([[z], [theta], [0.0], [0.0]])
     animation.update(state)
-    dataPlot.update(t, z, state, ctrl)
+    dataPlot.update(t, r, state, f)
 
-    #plt.show()
     t = t + P.t_plot  # advance time by t_plot
     plt.pause(0.1)
 
--- homework_template_folders/homework_template_folders/f_planar_vtol/python/hw2/VTOLSim.py
+++ homework_template_folders/homework_template_folders/f_planar_vtol/python/hw2/VTOLSim.py
@@ -2,34 +2,38 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 sys.path.append('..')  # add parent directory
-import massParam as P
+import VTOLParam as P
 from signalGenerator import signalGenerator
-from massAnimation import massAnimation
+from VTOLAnimation import VTOLAnimation
 from dataPlotter import dataPlotter
 
+
 # instantiate reference input classes
-reference = signalGenerator(amplitude=1.0, frequency=0.2, y_offset=0.1)
+reference = signalGenerator(amplitude=0.5, frequency=0.1)
 thetaRef = signalGenerator(amplitude=2.0*np.pi, frequency=0.1)
+phiRef = signalGenerator(amplitude=0.5, frequency=0.1)
 tauRef = signalGenerator(amplitude=5, frequency=.5)
 
 # instantiate the simulation plots and animation
 dataPlot = dataPlotter()
-animation = massAnimation()
+animation = VTOLAnimation()
 
 t = P.t_start  # time starts at t_start
 while t < P.t_end:  # main simulation loop
+
     # set variables
-    z = reference.sin(t)
-    ctrl = 0.0
+    r = reference.square(t)
+    theta = thetaRef.sin(t)
+    phi = phiRef.sin(t)
+    tau = tauRef.sawtooth(t)
 
     # update animation
-
-    state = np.array([[z], [0.0]])
+    state = np.array([[theta], [phi], [0.0], [0.0]])
     animation.update(state)
-    dataPlot.update(t, z, state, ctrl)
+    dataPlot.update(t, r, state, tau) # t,states, zref, href, force, torque
 
-    #plt.show()
-    t = t + P.t_plot  # advance time by t_plot
+    # advance time by t_plot
+    t = t + P.t_plot
     plt.pause(0.1)
 
 # Keeps the program from closing until the user presses a button.
